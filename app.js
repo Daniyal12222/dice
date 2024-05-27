a74284aa412c9d192158edfec52bc5081b1c8b6d
Daniyal ABBASI <153856061+Daniyal12222@users.noreply.github.com>
2024-05-27 08:33:39 +0500

    Dice

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ function dice() {
         alert('plzz enter a dice number')
     }else{
     if (random == 1) {
-        num = num +3
+        
         document.getElementById('co1').style.backgroundColor='red'
         document.getElementById('co2').style.backgroundColor='transparent'
         document.getElementById('co3').style.backgroundColor='transparent'
@@ -27,7 +27,7 @@ function dice() {
         document.getElementById('co4').style.backgroundColor='transparent'
         document.getElementById('co5').style.backgroundColor='transparent'
         document.getElementById('co6').style.backgroundColor='transparent'
-        num = num +3
+       
     }else if (random == 3){
         document.getElementById('co1').style.backgroundColor='transparent'
         document.getElementById('co2').style.backgroundColor='transparent'
@@ -35,7 +35,7 @@ function dice() {
         document.getElementById('co4').style.backgroundColor='transparent'
         document.getElementById('co5').style.backgroundColor='transparent'
         document.getElementById('co6').style.backgroundColor='transparent'
-        num = num +3
+       
     }else if (random == 4){
         document.getElementById('co1').style.backgroundColor='transparent'
         document.getElementById('co2').style.backgroundColor='transparent'
@@ -43,7 +43,7 @@ function dice() {
         document.getElementById('co4').style.backgroundColor='red'
         document.getElementById('co5').style.backgroundColor='transparent'
         document.getElementById('co6').style.backgroundColor='transparent'
-        num = num +3
+        
     }else if (random == 5){
         document.getElementById('co1').style.backgroundColor='transparent'
         document.getElementById('co2').style.backgroundColor='transparent'
@@ -51,7 +51,7 @@ function dice() {
         document.getElementById('co4').style.backgroundColor='transparent'
         document.getElementById('co5').style.backgroundColor='red'
         document.getElementById('co6').style.backgroundColor='transparent'
-        num = num +3
+        
     }else if (random == 6){
         document.getElementById('co1').style.backgroundColor='transparent'
         document.getElementById('co2').style.backgroundColor='transparent'
@@ -59,12 +59,13 @@ function dice() {
         document.getElementById('co4').style.backgroundColor='transparent'
         document.getElementById('co5').style.backgroundColor='transparent'
         document.getElementById('co6').style.backgroundColor='red'
-        num = num +3
+      
     }
     
     if (random == inputValo.value) {
         document.getElementById(`co${inputValo.value}`).style.backgroundColor='green'
         document.getElementById('loss').innerText= 'win'
+        num += 3
         
     }else{
         document.getElementById('loss').innerText= 'loss'
